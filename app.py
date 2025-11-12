fcbde4fd55bdc66914c42fa1ab4d6c33d21371ac
bustosjuliana <168046237+bustosjuliana@users.noreply.github.com>
2025-11-12 16:17:22 -0300

    endpoint pagos

--- app.py
+++ app.py
@@ -25,3 +25,15 @@ def pago_transaccion():
         logging.warning("Pago RECHAZADO (Status 409)")
         return flask.jsonify({"mensaje": "Pago fallido o rechazado"}), 409
 
+@app.route('/api/pagos/compensacion', methods=['POST'])
+def pago_compensacion():
+    logging.info("Recibida solicitud de COMPENSACIÓN de pago (Reembolso)")
+    simular_latencia()
+    
+    #el rembolso siempre va a ser exitoso
+    logging.info("Compensación de pago (reembolso) REALIZADA (Status 200)")
+    return flask.jsonify({"mensaje": "Compensación de pago (reembolso) realizada"}), 200
+
+if _name_ == '_main_':
+    # Corre puerto 5002
+    app.run(port=5002, debug=True)
